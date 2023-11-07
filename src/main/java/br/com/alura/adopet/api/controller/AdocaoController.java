9db9c921f4e2319109d1e3102fa3b1d8e0d5c0c8
vitoriajaco <vitoriajaco@gmail.com>
2023-11-07 10:42:14 -0300

    Limpando comentarios e imports nao utilizados

--- src/main/java/br/com/alura/adopet/api/controller/AdocaoController.java
+++ src/main/java/br/com/alura/adopet/api/controller/AdocaoController.java
@@ -2,20 +2,16 @@ package br.com.alura.adopet.api.controller;
 
 import br.com.alura.adopet.api.exception.ValidacaoException;
 import br.com.alura.adopet.api.model.Adocao;
-import br.com.alura.adopet.api.model.StatusAdocao;
-import br.com.alura.adopet.api.repository.AdocaoRepository;
+
 import br.com.alura.adopet.api.service.AdocaoService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
+
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.List;
+
 
 @RestController
 @RequestMapping("/adocoes")
--- src/main/java/br/com/alura/adopet/api/service/AdocaoService.java
+++ src/main/java/br/com/alura/adopet/api/service/AdocaoService.java
@@ -5,7 +5,7 @@ import br.com.alura.adopet.api.model.Adocao;
 import br.com.alura.adopet.api.model.StatusAdocao;
 import br.com.alura.adopet.api.repository.AdocaoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -23,23 +23,22 @@ public class AdocaoService {
     @Autowired
     private JavaMailSender emailSender;
 
-    public void solicitar(Adocao adocao){ //Precisa receber um parametro do tipo adocao
+    public void solicitar(Adocao adocao){
 
         if (adocao.getPet().getAdotado() == true) {
-            throw new ValidacaoException("Pet já foi adotado!"); //A classe de service nao é responsavel por passar o Response Entity e sim a controller
-//            return ResponseEntity.badRequest().body("Pet já foi adotado!"); -> Essa parte vai para o controller
+            throw new ValidacaoException("Pet já foi adotado!");
         } else {
             List<Adocao> adocoes = repository.findAll();
             for (Adocao a : adocoes) {
                 if (a.getTutor() == adocao.getTutor() && a.getStatus() == StatusAdocao.AGUARDANDO_AVALIACAO) {
                     throw new ValidacaoException("Tutor já possui outra adoção aguardando avaliação!");
-                    //  return ResponseEntity.badRequest().body("Tutor já possui outra adoção aguardando avaliação!");
+
                 }
             }
             for (Adocao a : adocoes) {
                 if (a.getPet() == adocao.getPet() && a.getStatus() == StatusAdocao.AGUARDANDO_AVALIACAO) {
                     throw new ValidacaoException("Pet já está aguardando avaliação para ser adotado!");
-                   // return ResponseEntity.badRequest().body("Pet já está aguardando avaliação para ser adotado!");
+
                 }
             }
             for (Adocao a : adocoes) {
@@ -49,7 +48,7 @@ public class AdocaoService {
                 }
                 if (contador == 5) {
                     throw new ValidacaoException("Tutor chegou ao limite máximo de 5 adoções!");
-                    //return ResponseEntity.badRequest().body("Tutor chegou ao limite máximo de 5 adoções!");
+
                 }
             }
         }
@@ -64,8 +63,7 @@ public class AdocaoService {
         email.setText("Olá " +adocao.getPet().getAbrigo().getNome() +"!\n\nUma solicitação de adoção foi registrada hoje para o pet: " +adocao.getPet().getNome() +". \nFavor avaliar para aprovação ou reprovação.");
         emailSender.send(email);
 
-      //  return ResponseEntity.ok().build(); Não vai ter esse retorno por o método ser void e nao ter retorno.
-        //Vai verificar se tem erro, se não tiver executa a regra de negócio.
+
     }
 
 
