e2af7ed8fa33294424aada9c310e5c610a220473
vitoriajaco <vitoriajaco@gmail.com>
2023-11-21 17:35:39 -0300

    Adicionando package e classes de validacao para limpeza de codigo

---
+++ src/main/java/br/com/alura/adopet/api/validacao/ValidacaoPetDisponivel.java
@@ -0,0 +1,23 @@
+package br.com.alura.adopet.api.validacao;
+
+import br.com.alura.adopet.api.dto.SolicitacaoAdocaoDto;
+import br.com.alura.adopet.api.exception.ValidacaoException;
+import br.com.alura.adopet.api.model.Pet;
+import br.com.alura.adopet.api.repository.PetRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component // Quando utiliza o component significa que usará um padrão de
+//injeção de dependencia e será elegivel para auto-configuração e autodeteccao de beans do spring. Esses beans são stateless
+public class ValidacaoPetDisponivel {
+
+    @Autowired
+    private PetRepository petRepository;
+
+    public void validar (SolicitacaoAdocaoDto dto){
+        Pet pet = petRepository.getReferenceById(dto.idPet());
+        if (pet.getAdotado()) {
+            throw new ValidacaoException("Pet já foi adotado!");
+        }
+    }
+}
